4b43c0bcda2e0669169e5ab232269b8a0f6adb70
250441100069MuhammadMaulanaRamadhan <samuraigaming109@gmail.com>
2026-05-21 20:25:16 +0700

    250441100069_MuhammadMaulanaRamadhan_Modul6_FikaNurlaila

---
+++ modul-6/250441100069_MuhammadMaulanaRamadhan/Soal1PbdModul6.sql
@@ -0,0 +1,12 @@
+SELECT AVG(nilai_angka) AS rata_rata
+FROM nilai;
+
+
+SELECT m.nim, m.nama, n.nilai_angka
+FROM mahasiswa m
+JOIN nilai n ON m.nim = n.nim
+WHERE n.nilai_angka >
+(
+    SELECT AVG(nilai_angka)
+    FROM nilai
+);
